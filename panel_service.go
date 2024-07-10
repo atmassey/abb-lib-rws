b26be1f4a603df07069bff4dc44f742720128424
Austin Massey <austin.massey@logan-aluminum.com>
2024-07-10 12:20:34 -0500

    restart function fix

--- panel_service.go
+++ panel_service.go
@@ -1,19 +1,24 @@
 package abb
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
+	"net/url"
 )
 
 // Restart the controller with any of the following actions: restart | istart | pstart | bstart
 func (c *Client) RestartController(Action string) error {
+	body := url.Values{}
+	body.Add("restart-mode", Action)
 	c.Client = c.DigestAuthenticate()
-	req, err := http.NewRequest("POST", "http://"+c.Host+"/rw/panel", nil)
+	req, err := http.NewRequest("POST", "http://"+c.Host+"/rw/panel", bytes.NewBufferString(body.Encode()))
 	if err != nil {
 		return err
 	}
 	q := req.URL.Query()
-	q.Add("restart-mode", Action)
+	q.Add("action", "restart")
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	req.URL.RawQuery = q.Encode()
 	resp, err := c.Client.Do(req)
 	if err != nil {
@@ -22,5 +27,6 @@ func (c *Client) RestartController(Action string) error {
 	if resp.StatusCode != http.StatusNoContent {
 		return fmt.Errorf("HTTP Status Code: %d", resp.StatusCode)
 	}
+	defer resp.Body.Close()
 	return nil
 }
