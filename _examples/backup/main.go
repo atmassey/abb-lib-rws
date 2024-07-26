fdaada6a7791bbf50d1e13ade70d09e27148edd6
Austin Massey <austin.massey@logan-aluminum.com>
2024-07-26 11:45:29 -0500

    delete old backups

--- _examples/backup/main.go
+++ _examples/backup/main.go
@@ -10,7 +10,13 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	//ftp download the backup to the local machine
 	err = GetDirectoryTree("hd0a/TEMP/my_test_directory", "./test")
+	if err != nil {
+		panic(err)
+	}
+	//delete the backup on the robot controller
+	err = client.DeleteDirectory("$TEMP/my_test_directory")
 	if err != nil {
 		panic(err)
 	}
