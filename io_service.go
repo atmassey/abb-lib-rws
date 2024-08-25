7f1da01c25cdf74c8469b579cba89ff1eea19d79
Austin Massey <austinmassey.am@gmail.com>
2024-08-25 05:36:31 -0500

    added update IO device function

--- io_service.go
+++ io_service.go
@@ -1,9 +1,11 @@
 package abb
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"net/url"
 
 	"github.com/atmassey/abb-lib-rws/structures"
 )
@@ -38,4 +40,33 @@ func (c *Client) GetIOSignals() (*structures.IOSignals, error) {
 		signals.SignalValue = append(signals.SignalValue, signal.Value)
 	}
 	return &signals, nil
+}
+
+// UpdateIODevice is used to enable or disable an IO device.
+// Possible values: {enable | disable}
+// Possible Device path example: Local/DRV_1
+func (c *Client) UpdateIODevice(State string, DevicePath string) error {
+	if State != "enable" && State != "disable" {
+		return fmt.Errorf("invalid state: %s", State)
+	}
+	body := url.Values{}
+	body.Add("lstate", State)
+	c.Client = c.DigestAuthenticate()
+	req, err := http.NewRequest("POST", "http://"+c.Host+"/rw/iosystem/devices/"+DevicePath, bytes.NewBufferString(body.Encode()))
+	if err != nil {
+		return err
+	}
+	q := req.URL.Query()
+	q.Add("action", "set")
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.URL.RawQuery = q.Encode()
+	resp, err := c.Client.Do(req)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != http.StatusNoContent {
+		return fmt.Errorf("HTTP Status Code: %d", resp.StatusCode)
+	}
+	defer closeErrorCheck(resp.Body)
+	return nil
 }
