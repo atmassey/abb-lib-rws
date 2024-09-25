721beeb6e3666caa7573a45cdc1189b786cab733
Austin Massey <austinmassey.am@gmail.com>
2024-09-25 06:39:52 -0500

    added clear profinet alarms

---
+++ profinet.go
@@ -0,0 +1,24 @@
+package abb
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// ClearProfinetAlarms clears the alarms for a specific profinet device
+func (c *Client) ClearProfinetAlarms(Device string) error {
+	c.Client = c.DigestAuthenticate()
+	req, err := http.NewRequest("POST", "http://"+c.Host+"rw/iosystem/devices/PROFINET/"+Device+"/alarms/clear", nil)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	resp, err := c.Client.Do(req)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("HTTP Status: %s", resp.Status)
+	}
+	return nil
+}
