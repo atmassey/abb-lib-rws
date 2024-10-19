1b28cf08feccbd591c61af5f8c9e6cc2b9678730
Austin Massey <austinmassey.am@gmail.com>
2024-10-19 06:22:42 -0500

    added reset accumulated energy function

--- system.go
+++ system.go
@@ -144,11 +144,33 @@ func (c *Client) RenameSystem(old_name string, new_name string) error {
 	}
 	q := req.URL.Query()
 	q.Add("action", "rename")
+	req.URL.RawQuery = q.Encode()
 	resp, err := c.Client.Do(req)
 	if err != nil {
 		return err
 	}
 	if resp.StatusCode != http.StatusCreated {
+		return fmt.Errorf("HTTP Status Code: %d", resp.StatusCode)
+	}
+	defer closeErrorCheck(resp.Body)
+	return nil
+}
+
+// ResetAccumulatedEnergy resets the accumulated energy consumption on the controller.
+func (c *Client) ResetAccumulatedEnergy() error {
+	c.Client = c.DigestAuthenticate()
+	req, err := http.NewRequest("POST", "http://"+c.Host+"/rw/system/energy", nil)
+	if err != nil {
+		return err
+	}
+	q := req.URL.Query()
+	q.Add("action", "reset")
+	req.URL.RawQuery = q.Encode()
+	resp, err := c.Client.Do(req)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != http.StatusNoContent {
 		return fmt.Errorf("HTTP Status Code: %d", resp.StatusCode)
 	}
 	defer closeErrorCheck(resp.Body)
