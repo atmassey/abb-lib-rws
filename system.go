8f65df094b1054ae517d8b266f780d35ded7907d
Austin Massey <austinmassey.am@gmail.com>
2024-10-20 08:43:00 -0500

    added release mastership function and added all to function names

--- system.go
+++ system.go
@@ -179,7 +179,7 @@ func (c *Client) ResetAccumulatedEnergy() error {
 
 // RequestMastership requests mastership of all domains on the controller.
 // ie. CFG, Motion, RAPID, etc.
-func (c *Client) RequestMastership() error {
+func (c *Client) RequestMastershipAll() error {
 	c.Client = c.DigestAuthenticate()
 	req, err := http.NewRequest("POST", "http://"+c.Host+"/rw/mastership", nil)
 	if err != nil {
@@ -189,6 +189,28 @@ func (c *Client) RequestMastership() error {
 	q.Add("action", "request")
 	req.URL.RawQuery = q.Encode()
 	resp, err := c.Client.Do(req)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != http.StatusNoContent {
+		return fmt.Errorf("HTTP Status Code: %d", resp.StatusCode)
+	}
+	defer closeErrorCheck(resp.Body)
+	return nil
+}
+
+// ReleaseMastership releases mastership of all domains on the controller.
+// ie. CFG, Motion, RAPID, etc.
+func (C *Client) ReleaseMastershipAll() error {
+	C.Client = C.DigestAuthenticate()
+	req, err := http.NewRequest("POST", "http://"+C.Host+"/rw/mastership", nil)
+	if err != nil {
+		return err
+	}
+	q := req.URL.Query()
+	q.Add("action", "release")
+	req.URL.RawQuery = q.Encode()
+	resp, err := C.Client.Do(req)
 	if err != nil {
 		return err
 	}
