43883548a77879b4b501cb7364cbc616f8ed7e5f
Austin Massey <austinmassey.am@gmail.com>
2024-10-30 04:45:27 -0500

    added set time server function

--- controller.go
+++ controller.go
@@ -497,3 +497,26 @@ func (c *Client) SetVTSpeed(speed int) error {
 	}
 	return nil
 }
+
+// SetTimeServer will set the time server of the controller
+func (c *Client) SetTimeServer(server string) error {
+	body := url.Values{}
+	body.Add("timeserver", server)
+	c.Client = c.DigestAuthenticate()
+	req, err := http.NewRequest("POST", "http://"+c.Host+"/ctrl/clock/timeserver", bytes.NewBufferString(body.Encode()))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	q := req.URL.Query()
+	q.Add("action", "set-timeserver")
+	req.URL.RawQuery = q.Encode()
+	resp, err := c.Client.Do(req)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != http.StatusNoContent {
+		return fmt.Errorf("HTTP Status Code: %d", resp.StatusCode)
+	}
+	return nil
+}
